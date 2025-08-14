0c95861a094fba904c78f6984d467153a25853b9
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2025-08-14 19:47:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
 // Driver Support App - Service Worker
 const CACHE_NAME = 'driver-support-v1.23.3-beta';
 const urlsToCache = [
-  './',
-  './index.html',
-  './js/app.js',
-  './js/data-manager.js',
-  './js/time-tracker.js',
-  './js/location-service.js',
-  './js/alerts.js',
-  './style.css',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/data-manager.js',
+  '/js/time-tracker.js',
+  '/js/location-service.js',
+  '/js/alerts.js',
+  '/style.css',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
 // Install event - cache resources
